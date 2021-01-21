6c97786759aebf0edf246ff3e08a633ca2a5663a
XeniyaVolkova <72569467+XeniyaVolkova@users.noreply.github.com>
2021-01-21 15:49:02 -0500

    Trie to fix the converson in the forecast

--- src/WeatherForecastPreview.js
+++ src/WeatherForecastPreview.js
@@ -1,80 +1,84 @@
 import React from "react";
 import "./BottomRow.css";
-
-
-export default function WeatherForecastPreview(props){
-function hours() {
-  let date= new Date(props.data.dt * 1000);
-  let hours= date.getHours();
-  return `${hours}:00`;
-}
- 
-    if (props.unit === "celsius") {
-        
+​
+export default function WeatherForecastPreview(props) {
+  function hours() {
+    let date = new Date(props.data.dt * 1000);
+    let hours = date.getHours();
+    return `${hours}:00`;
+  }
+​
+  if (props.unit === "celsius") {
     return (
       <div>
-           <h3>    
-            <div className="fiveDays">
-              {hours()}
-             </div>
-          </h3>
-           <div className="forecast-icons">
-             <img src={`/visuals/${props.data.weather[0].icon}.png`} alt={props.data.weather[0].description} width="40" />
-           </div>
-     
+        <h3>
+          <div className="fiveDays">{hours()}</div>
+        </h3>
+        <div className="forecast-icons">
+          <img
+            src={`/visuals/${props.data.weather[0].icon}.png`}
+            alt={props.data.weather[0].description}
+            width="40"
+          />
+        </div>
+​
         <div className="highs-lows ">
-       {Math.round(props.data.temp.max)}° / {Math.round(props.data.temp.min)}°</div>
-       </div>
+          {Math.round(props.data.main.temp_max)}° /{" "}
+          {Math.round(props.data.main.temp_min)}°
+        </div>
+      </div>
     );
-  } else{
-
-  return(
-    <div>
-   <h3>    
-            <div className="fiveDays">
-              {hours()}
-             </div>
-          </h3>
-           <div className="forecast-icons">
-             <img src={`/visuals/${props.data.weather[0].icon}.png`} alt={props.data.weather[0].description} width="40" />
-           </div>
-    
+  } else {
+    return (
+      <div>
+        <h3>
+          <div className="fiveDays">{hours()}</div>
+        </h3>
+        <div className="forecast-icons">
+          <img
+            src={`/visuals/${props.data.weather[0].icon}.png`}
+            alt={props.data.weather[0].description}
+            width="40"
+          />
+        </div>
+​
         <div className="highs-lows ">
-      {Math.round((props.data.temp.max * 9) / 5 + 32)}° / {Math.round((props.data.temp.min * 9) / 5 + 32)}°</div>
-   </div> );
+          {Math.round((props.data.main.temp_max * 9) / 5 + 32)}° /{" "}
+          {Math.round((props.data.main.temp_min * 9) / 5 + 32)}°
+        </div>
+      </div>
+    );
   }
-  
 }
 //The code that worked but the temperatures were not convertable
 //function temperature() {
-  //  let maxTemperature = Math.round(props.data.main.temp_max);
-    //let minTemperature = Math.round(props.data.main.temp_min);
-    //if (props.unit === "fahrenheit") {
-      //maxTemperature = Math.round((maxTemperature *9)/5 +32);
-      //minTemperature = Math.round((minTemperature * 9)/5 + 32);
-    //}
-    //return (
-     // <div className="temperature">
-      // {maxTemperature}° / {minTemperature}°</div>
-    //);
-  //}
-
-
-  //return(
-    //<div className="WeatherForecastPreview col">
-      //    <h3>    
-        //    <div className="fiveDays">
-          //    {hours()}
-            // </div>
-         // </h3>
-          // <div className="forecast-icons">
-            // <img src={`/visuals/${props.data.weather[0].icon}.png`} alt={props.data.weather[0].description} width="40" />
-           //</div>
-      //<div className="highs-lows">
-       //<span>     
-       //{temperature()}        
-       //</span>
-     //</div>
-    //</div>
-  //)
+//  let maxTemperature = Math.round(props.data.main.temp_max);
+//let minTemperature = Math.round(props.data.main.temp_min);
+//if (props.unit === "fahrenheit") {
+//maxTemperature = Math.round((maxTemperature *9)/5 +32);
+//minTemperature = Math.round((minTemperature * 9)/5 + 32);
+//}
+//return (
+// <div className="temperature">
+// {maxTemperature}° / {minTemperature}°</div>
+//);
+//}
+​
+//return(
+//<div className="WeatherForecastPreview col">
+//    <h3>
+//    <div className="fiveDays">
+//    {hours()}
+// </div>
+// </h3>
+// <div className="forecast-icons">
+// <img src={`/visuals/${props.data.weather[0].icon}.png`} alt={props.data.weather[0].description} width="40" />
+//</div>
+//<div className="highs-lows">
+//<span>
+//{temperature()}
+//</span>
+//</div>
+//</div>
+//)
 //}
--- src/WeatherTemperature.js
+++ src/WeatherTemperature.js
@@ -1,55 +1,52 @@
-import React, {useState} from "react";
+import React from "react";
 import "./Weather.css";
-
-
-export default function WeatherTemperature(props){
-const [unit, setUnit] = useState("celsius");
+​
+export default function WeatherTemperature(props) {
   function showFahrenheit(event) {
     event.preventDefault();
-    setUnit("fahrenheit");
-   
+    props.setUnit("fahrenheit");
   }
-
+​
   function showCelsius(event) {
     event.preventDefault();
-    setUnit("celsius");
-    
+    props.setUnit("celsius");
   }
-
+​
   function fahrenheit() {
     return (props.celsius * 9) / 5 + 32;
   }
   function feelFahrenheit() {
     return (props.celsiusFeel * 9) / 5 + 32;
   }
-
-  if (unit === "celsius") {
+​
+  if (props.unit === "celsius") {
     return (
-    <div className="weatherTemperature">
-        <p>Temperature: {Math.round(props.celsius)}
-        <span className="unit">
-          °C |{" "}
-          <a href="/" onClick={showFahrenheit}>
-            °F
-          </a>
-          <br />
-          <p>Feels like: {props.celsiusFeel}°</p>
-        </span>
+      <div className="weatherTemperature">
+        <p>
+          Temperature: {Math.round(props.celsius)}
+          <span className="unit">
+            °C |{" "}
+            <a href="/" onClick={showFahrenheit}>
+              °F
+            </a>
+            
+          </span>
         </p>
       </div>
     );
   } else {
     return (
-     <div className="weatherTemperature">
-        <p>Temperature: {Math.round(fahrenheit())}
-        <span className="unit">
-          <a href="/" onClick={showCelsius}>
-            °C
-          </a>{" "}
-          | °F
-        </span>
-       </p>
-         <p>Feels like: {Math.round(feelFahrenheit())}°</p>
+      <div className="weatherTemperature">
+        <p>
+          Temperature: {Math.round(fahrenheit())}
+          <span className="unit">
+            <a href="/" onClick={showCelsius}>
+              °C
+            </a>{" "}
+            | °F
+          </span>
+        </p>
+        
       </div>
     );
   }
